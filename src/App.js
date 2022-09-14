2aea4a4f2a2fa11682af6e9b4e7b7b7487889a56
AirisRX <airidas.rupsas@ku.lt>
2022-09-14 13:46:55 +0300

    Added fonts, cleared up code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.png';
 import './App.css';
+import React from "react";
+import background from "./background.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Bundle</p>
-        <a>Work in progress</a>
+      <header className="App-header" style={{ backgroundImage: `url(${background})` }}>
+
       </header>
     </div>
   );
