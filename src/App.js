bbbf6f4a8e49007c7b40d1c5dd17c5ca5ecbe8e2
Andrew1415 <andrius1415@gmail.com>
2022-09-15 14:44:25 +0300

    Both favicon and logo have been converted to SVG

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.png';
 import './App.css';
+import logo from './images/logo.png';
+import MainLogo from './images/logo2.svg';
 
 function App() {
   return (
+    
     <div className="App">
+
+
+      
+      <div className="Loading">
+        <img src={MainLogo} className="Main-logo" alt="logo"/>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>Bundle</p>
-        <a>Work in progress</a>
+        <p>Work in progress</p>
       </header>
     </div>
   );
 }
+
 
 export default App;
